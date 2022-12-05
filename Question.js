23245c071e5ee818100c043c0d5ffc9eb468963e
reaanv <108709514+reaanv@users.noreply.github.com>
2022-12-05 17:59:16 +0400

    Update Question.js

--- Question.js
+++ Question.js
@@ -46,14 +46,14 @@ class Question {
 
     this.button.mousePressed(()=>{
       
-      // this.message.html("Thank You, Your Answer Has Been Submitted");
-      // this.message.position(350, 350);
+       this.message.html("Thank You, Your Answer Has Been Submitted");
+       this.message.position(350, 350);
 
       // this.message.html("Thank You, Your Answer Has Been Submitted");
       // this.message.position();
 
-       this.message("Thank You, Your Answer Has Been Submitted");
-       this.message(350, 350);
+       //this.message("Thank You, Your Answer Has Been Submitted");
+      // this.message(350, 350);
 
       // this.message.html("Thank You, Your Answer Has Been Submitted");
       // this.message.position(350);
